221b05eb3cadcbc936e02fcd5412d1b5c8e1f857
luigiPisoni <luigipisoni04@gmail.com>
2024-10-22 22:25:35 -0300

    inicio back

---
+++ trabalho-integrador-back/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+const port = 3001;
+
+server.listen(port, () => {
+  console.log("Server iniciado no port " + port);
+});
