4f2016f1ddcd5a32e88f569bd1c687d8bb1e07ae
JoaoKanivete <joaok670@gmail.com>
2024-10-22 22:40:57 -0300

    rota novo-prato

--- trabalho-integrador-back/index.js
+++ trabalho-integrador-back/index.js
@@ -10,3 +10,22 @@ const port = 3001;
 server.listen(port, () => {
   console.log("Server iniciado no port " + port);
 });
+
+server.post("/novo-prato", (req, res) => {
+  console.log("tá chegando");
+
+  var prato = req.body;
+
+  if (
+    prato.nome.length < 3 ||
+    parseFloat(prato.preco) <= 0 ||
+    prato.ingredientes.length < 1
+  ) {
+    // CASO O PRATO SEJA INVÁLIDO
+    res.status(400).send("Prato inválido!!!");
+  } else {
+    res.send("Prato cadastrado!!!");
+  }
+
+  res.send();
+});
