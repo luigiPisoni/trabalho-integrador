7dfc6a3cd01474bdd301178471e9e0f680298bfd
luigiPisoni <luigipisoni04@gmail.com>
2024-11-24 03:23:22 -0300

    header responsivo

--- trabalho-integrador-front/src/components/Header.jsx
+++ trabalho-integrador-front/src/components/Header.jsx
@@ -1,26 +1,29 @@
 import { Wheat } from "lucide-react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import React, { useState } from "react";
 
 function Header() {
   const [isLogged, setIsLogged] = useState(false);
 
-  const handleClick = () => {
+  const handleNovoPedido = () => {
     setIsLogged(!isLogged);
   };
+
   return (
-    <header className="py-7 px-24 sticky flex w-full top-0 z-50 items-center gap-12">
+    <header className="px-8 md:px-24 py-4 sm:py-7 sticky flex w-full top-0 z-50 items-center gap-6 md:gap-12 bg-white ">
       {/* Container da logo e título */}
       <div className="flex">
-        <Wheat color="#426B1F" strokeWidth={"1.75px"} />
-        <p className="ml-3 text-3xl text-default-green font-serif">
-          Doof House
-        </p>
+        {/* <Wheat
+          className="mr-3 sm:hidden"
+          color="#426B1F"
+          strokeWidth={"1.75px"}
+        /> */}
+        <p className="text-3xl text-default-green font-serif">Food House</p>
       </div>
       <div className="ml-auto flex gap-5">
-        <button className="hover:border-b-2 border-default-green">
+        {/* <button className="hover:border-b-2 border-default-green">
           Meu perfil
-        </button>
+        </button> */}
         {isLogged && (
           <button className="hover:border-b-2 border-default-green">
             Gerenciar
@@ -29,7 +32,7 @@ function Header() {
       </div>
       <div>
         <button
-          onClick={handleClick}
+          onClick={handleNovoPedido}
           className="bg-default-green p-3 text-white rounded-md hover:opacity-75"
         >
           Novo pedido
