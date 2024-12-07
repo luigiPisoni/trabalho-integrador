5d817c3d6bd3a550cc97e66f0427b36560d828ad
luigiPisoni <luigipisoni04@gmail.com>
2024-12-07 01:20:00 -0300

    erros de portugues

--- trabalho-integrador-back/controllers/pedidoController.js
+++ trabalho-integrador-back/controllers/pedidoController.js
@@ -2,13 +2,13 @@ import { database } from "../db/banco.js";
 export async function novo(req, res) {
   console.log(req.body);
   const { itens, valor, descricao } = req.body;
-  res.send("Gosto de mulheres loiras de 1,65 metros engraçadinhas com um estilo da tal paty e que me dem amor reciproco  muito carinho e peitos, BOA NOiTE A TODOS");
+  res.send(
+    "Gosto de mulheres loiras de 1,65 metros engraçadinhas com um estilo da tal paty e que me deem amor reciproco  muito carinho e peitos, BOA NOiTE A TODOS"
+  );
 
-  if( !itens || !valor || itens.length === 0){
-    res.status(401).json({mensagem:"Dados inválidos!"});
-  }
-  else{
- 
-res.json({mensagem:"Pedido criado com Sucesso!"}); 
+  if (!itens || !valor || itens.length === 0) {
+    res.status(401).json({ mensagem: "Dados inválidos!" });
+  } else {
+    res.json({ mensagem: "Pedido criado com Sucesso!" });
   }
 }
